48acb42fb5a7e8ef98f6ff474343b797379fdcff
정시현 <54366260+jungsiroo@users.noreply.github.com>
2024-03-04 07:39:36 +0900

    Title: 그래프 탐색, Time: 131ms, Memory: 26MB, Status: Passed - CodeTree

--- 240304/그래프 탐색/graph-traversal.py
+++ 240304/그래프 탐색/graph-traversal.py
@@ -1,26 +1,24 @@
 import sys
 input = sys.stdin.readline
 
-def dfs(now, count):
-    global visited
+def dfs(now):
+    global visited, answer
 
-    if visited[now]:
-        return count
-
-    answer = 0
     for node in adj[now]:
-        visited[node] = True
-        answer += dfs(node, count+1)
+        if visited[node]:
+            continue
 
-    return answer
+        visited[node] = True
+        answer += 1
+        dfs(node)
 
 
 if __name__ == "__main__":
     n, m = map(int, input().split())
 
     global visited, answer
-
     answer =0 
+
     adj = [[] for _ in range(n+1)]
     visited = [False]*(n+1)
 
@@ -29,4 +27,6 @@ if __name__ == "__main__":
         adj[x].append(y)
         adj[y].append(x)
 
-    print(dfs(1, 0))
+    visited[1] = True
+    dfs(1)
+    print(answer)
