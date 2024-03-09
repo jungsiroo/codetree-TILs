7c60279c8945b3eea06ee2c36380f9521162ff38
정시현 <54366260+jungsiroo@users.noreply.github.com>
2024-03-09 09:56:17 +0900

    Title: 최대 숫자 만들기, Time: 291ms, Memory: 31MB, Status: Passed - CodeTree

--- 240309/최대 숫자 만들기/make-biggest-num.py
+++ 240309/최대 숫자 만들기/make-biggest-num.py
@@ -3,7 +3,7 @@ input = sys.stdin.readline
 
 if __name__ == "__main__":
     n = int(input())
-    nums = [str(input()) for _ in range(n)]
+    nums = [str(input().strip()) for _ in range(n)]
     nums.sort(key=lambda x:x*9, reverse=True)
 
     print(''.join(nums))
