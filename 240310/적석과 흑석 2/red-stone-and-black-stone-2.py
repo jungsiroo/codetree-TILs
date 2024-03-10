26bf96490e393116a43470201fb57973a7f3694d
정시현 <54366260+jungsiroo@users.noreply.github.com>
2024-03-10 23:03:41 +0900

    Title: 적석과 흑석 2, Time: 368ms, Memory: 30MB, Status: Wrong Answer - CodeTree

---
+++ 240310/적석과 흑석 2/red-stone-and-black-stone-2.py
@@ -0,0 +1,54 @@
+import sys
+from heapq import heappush, heappop
+input = sys.stdin.readline
+
+if __name__ == "__main__":
+    c, n = map(int, input().split())
+
+    red, black = [], []
+
+    for _ in range(c):
+        red.append(int(input()))
+
+    red.sort()
+
+    for _ in range(n):
+        a, b = map(int, input().split())
+        black.append([a, b])
+    
+    black.sort(key=lambda x:(x[1], x[0]))
+
+    pre_answer = 0
+    r_idx, b_idx = c-1, n-1
+
+    while r_idx>=0 and b_idx>=0:
+        t = red[r_idx]
+        a, b = black[b_idx]
+
+        if a<=t<=b:
+            pre_answer += 1
+            b_idx -= 1
+            r_idx -= 1
+        elif t<a:
+            b_idx -= 1
+        elif t>b:
+            r_idx -= 1
+
+    r_idx, b_dix = 0, 0
+    answer = 0
+
+    while r_idx<c and b_idx<n:
+        t = red[r_idx]
+        a, b = black[b_idx]
+
+        if a<=t<=b:
+            answer += 1
+            b_idx += 1
+            r_idx += 1
+        elif t<a:
+            b_idx += 1
+        elif t>b:
+            r_idx += 1
+    
+        
+    print(max(answer, pre_answer))
