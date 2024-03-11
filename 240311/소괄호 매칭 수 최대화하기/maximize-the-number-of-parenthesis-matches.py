aaa9da0a7caf0f3fac85c9d7aa857e5dc485d449
정시현 <54366260+jungsiroo@users.noreply.github.com>
2024-03-11 19:44:36 +0900

    Title: 소괄호 매칭 수 최대화하기, Time: 142ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240311/소괄호 매칭 수 최대화하기/maximize-the-number-of-parenthesis-matches.py
@@ -0,0 +1,34 @@
+import sys
+from heapq import heappop, heappush
+input = sys.stdin.readline
+
+def calculate_score(bracket):
+    dp = [0] * len(bracket)
+    dp[0] = int(bracket[0] == ")")
+
+    for i in range(1, len(bracket)):
+        if bracket[i] == ")":
+            dp[i] = dp[i-1] + 1
+    
+    answer = 0
+
+    for i in range(len(bracket)):
+        if bracket[i] == "(":
+            answer += (dp[-1] - dp[i])
+    
+    return answer
+    
+if __name__ == "__main__":
+    n = int(input())
+    strings = []
+
+    for _ in range(n):
+        string = input().strip()
+        heappush(strings, [-string.count('(')/len(string), string])
+
+    bracket = '' 
+    while strings:
+        _, string = heappop(strings)
+        bracket += string
+
+    print(calculate_score(bracket))
