1b47b4656430d7898d32efaee500ff759f41906b
정시현 <54366260+jungsiroo@users.noreply.github.com>
2024-03-11 20:52:04 +0900

    Title: 회의실 겹치지 않게 하기, Time: 365ms, Memory: 34MB, Status: Wrong Answer - CodeTree

--- 240311/회의실 겹치지 않게 하기/do-not-overlap-the-meeting-room.py
+++ 240311/회의실 겹치지 않게 하기/do-not-overlap-the-meeting-room.py
@@ -16,12 +16,14 @@ if __name__ == "__main__":
         if start>=dp[i-1][1]:
             dp[i] = [start, end, dp[i-1][2]+1]
         else:
-            index = 0
+            index = -1
             for j in range(i, -1, -1):
                 if dp[j][1]<=start:
                     index =  j
                     break
-
+            
             dp[i] = [start, end, dp[index][2]+1]
+            if index == -1:
+                dp[i] = [start, end, 1]
 
     print(n-dp[n-1][2])
