2cf38d32e6a50dfbf0e09523e78311eebc026e64
정시현 <54366260+jungsiroo@users.noreply.github.com>
2024-03-13 19:02:20 +0900

    Title: 연속된 숫자 만들기 2, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240313/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
@@ -0,0 +1,31 @@
+import sys
+input = sys.stdin.readline
+
+if __name__ == "__main__":
+    arr = list(map(int, input().split()))
+    arr.sort()
+
+    l_dist, r_dist = arr[1]-arr[0], arr[2]-arr[1]
+
+    choice = -1
+    answer = 0
+
+    while l_dist != 1 or r_dist != 1:
+        if l_dist == 1:
+            choice = 0
+        elif r_dist == 1:
+            choice = 2
+        else:
+            if l_dist >= r_dist:
+                choice = 0
+            else:
+                choice = 2
+        
+        mid = (arr[1]+arr[2-choice]) // 2
+        arr[choice] = arr[1]
+        arr[1] = mid
+
+        l_dist, r_dist = arr[1]-arr[0], arr[2]-arr[1]
+        answer += 1
+    
+    print(answer)
