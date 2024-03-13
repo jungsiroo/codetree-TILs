64b3756df2c6d363050445aa35f1d339357e5438
정시현 <54366260+jungsiroo@users.noreply.github.com>
2024-03-13 21:47:13 +0900

    Title: 두 번째로 작은 수의 위치, Time: 129ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240313/두 번째로 작은 수의 위치/location-of-the-second-smallest-number.py
@@ -0,0 +1,26 @@
+import sys
+from collections import defaultdict
+input = sys.stdin.readline
+
+def solution(key):
+    if key == -1 or key not in order or len(order[key])>1:
+        return -1
+    return order[key]
+
+if __name__ == "__main__":
+    n = int(input())
+    nums = list(map(int, input().split()))
+    order = defaultdict(list)
+
+    for i in range(n):
+        order[nums[i]].append(i+1)
+
+    nums.sort()
+    smallest = nums[0]
+
+    key = -1
+    for i in range(n):
+        if nums[i] > smallest:
+            key = nums[i]
+
+    print(solution(key))
