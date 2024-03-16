f49fa3d32509816bb647692c97fbb6fa0cc64505
정시현 <54366260+jungsiroo@users.noreply.github.com>
2024-03-16 18:51:06 +0900

    Title: 초대장과 번호표, Time: 149ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240316/초대장과 번호표/invitation-and-number-tag.py
+++ 240316/초대장과 번호표/invitation-and-number-tag.py
@@ -1,4 +1,5 @@
 import sys
+from collections import deque
 input = sys.stdin.readline
 
 if __name__ == "__main__":
@@ -6,12 +7,24 @@ if __name__ == "__main__":
     visited = set([1])
 
     answer = 0
+    save = deque()
+
     for _ in range(g):
         inp = list(map(int, input().split()))
         total, people = inp[0], set(inp[1:])
+        save.append([total, people, 0, 0])
 
-        if total - len(visited.intersection(people)) == 1:
+    while save:
+        total, people, now, prev = save.popleft()
+
+        if (now == prev) and (now+prev > 0):
+            continue
+        
+        inter = len(visited.intersection(people))
+        if total - inter == 1:
             visited.update(people)
-            # print(visited)
+        else:
+            save.append([total, people, inter, now])
+        # print(visited)
 
     print(len(visited))
