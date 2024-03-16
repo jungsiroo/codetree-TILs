37fabb0b101a7e72f8461be93f526c648585cb77
정시현 <54366260+jungsiroo@users.noreply.github.com>
2024-03-16 18:54:47 +0900

    Title: 대칭 차집합, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240316/대칭 차집합/symmetric-difference-set.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+if __name__ == "__main__":
+    n, m = map(int, input().split())
+
+    a = set(map(int, input().split()))
+    b = set(map(int, input().split()))
+
+    answer = (a-b).add(b-a)
+    print(len(answer))
