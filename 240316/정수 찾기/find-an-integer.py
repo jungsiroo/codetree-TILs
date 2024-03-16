a8e95f0c4e2ef3b1713ead3970824b37989c486c
정시현 <54366260+jungsiroo@users.noreply.github.com>
2024-03-16 14:48:28 +0900

    Title: 정수 찾기, Time: 223ms, Memory: 36MB, Status: Passed - CodeTree

---
+++ 240316/정수 찾기/find-an-integer.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+if __name__ == "__main__":
+    n = int(input())
+    arr1 = list(map(int, input().split()))
+    arr1 = set(arr1)
+
+    m = int(input())
+    arr2 = list(map(int, input().split()))
+
+    for num in arr2:
+        print(int(num in arr1))
