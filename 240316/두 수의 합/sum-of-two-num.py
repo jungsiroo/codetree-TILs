498b52af17499d12aac2a1341ced8eb4ad34b569
정시현 <54366260+jungsiroo@users.noreply.github.com>
2024-03-16 12:51:19 +0900

    Title: 두 수의 합, Time: 131ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240316/두 수의 합/sum-of-two-num.py
+++ 240316/두 수의 합/sum-of-two-num.py
@@ -5,18 +5,17 @@ if __name__ == "__main__":
     n, k = map(int, input().split())
     nums = list(map(int, input().split()))
 
-    hash_map = set()
+    hash_map = dict()
 
     for num in nums:
-        hash_map.add(num)
+        hash_map[num] = hash_map.get(num, 0) + 1
 
     answer = 0
-    for num in nums:
-        target = k - num
 
-        if target in hash_map:
-            answer += 1
-            hash_map.remove(num)
-            hash_map.remove(target)
+    for num in nums:
+        target = k- num
+        answer += hash_map.get(target, 0)
+        hash_map[num] = max(hash_map.get(num,0) -1, 0)
+
 
     print(answer)
