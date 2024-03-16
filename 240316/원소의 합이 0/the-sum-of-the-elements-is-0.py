9b9fd141bee6de017bbc7dcab085b32d1aceb993
정시현 <54366260+jungsiroo@users.noreply.github.com>
2024-03-16 14:12:37 +0900

    Title: 원소의 합이 0, Time: 103ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240316/원소의 합이 0/the-sum-of-the-elements-is-0.py
@@ -0,0 +1,37 @@
+import sys
+input = sys.stdin.readline
+
+if __name__ == "__main__":
+    n = int(input())
+
+    nums = [[] for _ in range(4)]
+
+    for i in range(4):
+        nums[i] = list(map(int, input().split()))
+
+    ab_pair = dict()
+
+    for i in range(n):
+        a_value = nums[0][i]
+        for j in range(n):
+            b_value = nums[1][j]
+            ab_pair[(a_value + b_value)] = ab_pair.get(a_value+b_value, 0)+1
+
+    cd_pair = dict()    
+
+    for i in range(n):
+        c_value = nums[2][i]
+        for j in range(n):
+            d_value = nums[3][j]
+            cd_pair[(c_value + d_value)] = cd_pair.get(c_value+d_value, 0)+1
+
+    answer = 0
+    for ab_val in ab_pair.keys():
+        ab_pair[ab_val] -= 1
+
+        for cd_val, cd_cnt in cd_pair.items():
+            if ab_val + cd_val == 0:
+                add_up = min(ab_pair[ab_val], cd_cnt)
+                answer += add_up
+                cd_pair[cd_val] -= add_up
+    print(answer)
